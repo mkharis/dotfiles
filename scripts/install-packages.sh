f7e5bef1486bddf0a282920931dd079c5f584f25
CoreOS Admin <kharis@kharis-macbook.local>
2026-09-02 20:53:40 +0700

    Add install packages script

---
+++ scripts/install-packages.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# enable repositories
+sudo dnf copr enable atim/lazygit -y
+sudo dnf copr enable atim/starship -y
+
+# upgrade packages
+dnf upgrade -y
+
+# install packages
+sudo dnf install -y \
+    atuin \
+    eza \
+    direnv \
+    fd-find \
+    fzf \
+    gh \
+    jq \
+    lazygit \
+    neovim \
+    nodejs \
+    ripgrep \
+    starship \
+    stow \
+    tmux \
+    zoxide \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting
